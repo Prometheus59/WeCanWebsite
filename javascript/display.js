66f52a045ba03973bcdfd166e261e8cb064b977d
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-05-31 09:32:56 -0400

    structure for service page's menu created

--- javascript/display.js
+++ javascript/display.js
@@ -1,8 +1,32 @@
 document.getElementById('auto').addEventListener("click", function() {
-    var disp = document.getElementById('display').style.display;
-    if (disp === 'none'){
-        document.getElementById('display').style.display = 'inherit ';
-    } else {
-        document.getElementById('display').style.display = 'none';
-    }
+    var all = document.querySelectorAll(".display");
+    var disp = document.getElementById('car').style.display;
+
+    // Clear all descriptions
+    all.forEach(function(elem) {
+        elem.style.display = 'none';
+    });
+    document.getElementById('car').style.display='block';
+});
+
+document.getElementById('home').addEventListener("click", function() {
+    var all = document.querySelectorAll(".display");
+    var disp = document.getElementById('house').style.display;
+
+    // Clear all descriptions
+    all.forEach(function(elem) {
+        elem.style.display = 'none';
+    });
+    document.getElementById('house').style.display='block';
+});
+
+document.getElementById('comm').addEventListener("click", function() {
+    var all = document.querySelectorAll(".display");
+    var disp = document.getElementById('stuff').style.display;
+
+    // Clear all descriptions
+    all.forEach(function(elem) {
+        elem.style.display = 'none';
+    });
+    document.getElementById('stuff').style.display='block';
 });
